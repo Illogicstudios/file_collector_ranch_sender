477ab0dd641c8a388d6a2add23c82eec87df931f
mariusjenin <marius2401@laposte.net>
2023-05-09 10:40:32 +0200

    update copy to distant

--- copy_to_distant.py
+++
@@ -1,9 +0,0 @@
-import importlib
-from common import utils
-
-utils.unload_packages(silent=True, package="file_collector_ranch_sender")
-importlib.import_module("file_collector_ranch_sender")
-import CollectorCopier
-from file_collector_ranch_sender.CollectorCopier import CollectorCopier
-collector_copier = CollectorCopier()
-collector_copier.run_copy(sys.argv[1])
